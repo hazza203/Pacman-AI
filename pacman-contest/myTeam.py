1c9793a607bf834219c28ba460f2526eb4b370bc
Hazza203 <s3383212@student.rmit.edu.au>
2018-05-09 18:31:26 +1000

    Pushed demo stuff i worked on

--- pacman-contest/myTeam.py
+++ pacman-contest/myTeam.py
@@ -22,7 +22,7 @@ import game
 #################
 
 def createTeam(firstIndex, secondIndex, isRed,
-               first = 'DummyAgent', second = 'DummyAgent'):
+               first = 'DefensiveAgent', second = 'DummyAgent'):
   """
   This function should return a list of two agents that will form the
   team, initialized using firstIndex and secondIndex as their agent
@@ -90,3 +90,126 @@ class DummyAgent(CaptureAgent):
 
     return random.choice(actions)
 
+##########
+# Agents #
+##########
+
+class DefensiveAgent(CaptureAgent):
+  """
+  A Dummy agent to serve as an example of the necessary agent structure.
+  You should look at baselineTeam.py for more details about how to
+  create an agent as this is the bare minimum.
+  """
+
+  def __init__(self, index):
+      CaptureAgent.__init__(self, index)
+      self.coordinates = dict()
+      self.at_center = False
+      self.at_top = False
+      self.at_bottom = False
+
+  def registerInitialState(self, gameState):
+    """
+    This method handles the initial setup of the
+    agent to populate useful fields (such as what team
+    we're on).
+
+    A distanceCalculator instance caches the maze distances
+    between each pair of positions, so your agents can use:
+    self.distancer.getDistance(p1, p2)
+
+    IMPORTANT: This method may run for at most 15 seconds.
+    """
+    self.coordinates = dict()
+
+    self.coordinates['height'] = len(gameState.getWalls()[0])
+
+    width = 0
+    for wall in gameState.getWalls().asList():
+        if wall[1] == 0:
+            width += 1
+
+    print width
+    width = (width / 2) - 4 if gameState.isOnRedTeam(self.index) else (width / 2) + 4
+    print width
+
+    self.coordinates['width'] = width
+    self.coordinates['center'] = width, self.coordinates['height'] / 2
+    self.coordinates['top_center'] = width, self.coordinates['center'][1] + self.coordinates['height'] / 3
+    self.coordinates['bottom_center'] = width, self.coordinates['center'][1] - self.coordinates['height'] / 3
+
+    while gameState.hasWall(self.coordinates['center'][0],self.coordinates['center'][1]):
+        width = width + 1 if gameState.isOnRedTeam(self.index) else width + 1
+        self.coordinates['center'] = width, self.coordinates['height'] / 2
+
+    while gameState.hasWall(self.coordinates['top_center'][0], self.coordinates['top_center'][1]):
+        width = width + 1 if gameState.isOnRedTeam(self.index) else width + 1
+        self.coordinates['top_center'] = width, self.coordinates['center'][1] + self.coordinates['height'] / 3
+
+    while gameState.hasWall(self.coordinates['bottom_center'][0], self.coordinates['bottom_center'][1]):
+        width = width + 1 if gameState.isOnRedTeam(self.index) else width + 1
+        self.coordinates['bottom_center'] = width, self.coordinates['center'][1] - self.coordinates['height'] / 3
+
+    #self.coordinates['']
+
+    '''
+    Make sure you do not delete the following line. If you would like to
+    use Manhattan distances instead of maze distances in order to save
+    on initialization time, please take a look at
+    CaptureAgent.registerInitialState in captureAgents.py.
+    '''
+    CaptureAgent.registerInitialState(self, gameState)
+
+    '''
+    Your initialization code goes here, if you need any.
+    '''
+
+  def chooseAction(self, gameState):
+    """
+    Picks among actions randomly.
+    """
+
+    actions = gameState.getLegalActions(self.index)
+    best_action = None
+    min = 9999
+    enemy_dists = gameState.getAgentDistances()
+    print enemy_dists
+    for enemy in enemy_dists:
+        if enemy < 5:
+            for action in actions:
+                nextState = gameState.generateSuccessor(self.index, action)
+                enemy_dists.
+
+    if not self.at_center:
+        self.at_center = True if gameState.getAgentPosition(self.index) == self.coordinates['center'] else False
+        best_action = self.get_best_action_for_pos(gameState, actions, self.coordinates['center'])
+        return best_action if best_action is not None else random.choice(actions)
+
+    elif not self.at_top:
+        best_action = self.get_best_action_for_pos(gameState, actions, self.coordinates['top_center'])
+        nextState = gameState.generateSuccessor(self.index, best_action)
+        if nextState.getAgentPosition(self.index) == self.coordinates['top_center']:
+            self.at_top = True
+            self.at_bottom = False
+        return best_action if best_action is not None else random.choice(actions)
+
+    elif not self.at_bottom:
+        best_action = self.get_best_action_for_pos(gameState, actions, self.coordinates['bottom_center'])
+        nextState = gameState.generateSuccessor(self.index, best_action)
+        if nextState.getAgentPosition(self.index) == self.coordinates['bottom_center']:
+            self.at_top = False
+            self.at_bottom = True
+        return best_action if best_action is not None else random.choice(actions)
+
+    return random.choice(actions)
+
+  def get_best_action_for_pos(self, gameState, actions, pos):
+      min = 9999
+      best_action = None
+      for action in actions:
+          nextState = gameState.generateSuccessor(self.index, action)
+          dist = self.getMazeDistance(nextState.getAgentPosition(self.index), pos)
+          if dist < min:
+              min = dist
+              best_action = action
+      return best_action
